f208f2d5f4d1ce50cfca27984dd0d47d7472ed2e
Ashley Hacker <ashleyhacker0127@gmail.com>
2025-06-23 19:51:54 -0400

    Update LeahForSM64.js

--- src/lib/FileJS/LeahForSM64.js
+++ src/lib/FileJS/LeahForSM64.js
@@ -5,7 +5,7 @@ export default {
         src: srcimg,
         alt: 'Gif showing Leah the Bunny in SM64coopdx'
     },
-    href: 'https://github.com/Bnuey/LockTilesKeybind',
+    href: 'src/lib/files/char-select-leah.zip',
     title: 'Leah The Bunny',
     date: '4/5/25',
     description: "For SM64coopdx<br>Play as everyone's favourite bnuey",
